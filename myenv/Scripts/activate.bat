32a35e35845dc2a4ad2271dfa1966578225657d4
Muhd Sufi <syufi.sas@gmail.com>
2024-10-28 12:01:33 +0800

    debug streamlit

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Documents\AI Bootcamp 2024\Capstone_Project\myenv
+set VIRTUAL_ENV=C:\Users\User\Documents\AI Bootcamp 2024\Capstone_Project-Final\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
